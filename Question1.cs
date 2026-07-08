3fe5c48f58cf03f3e774a118c878b6a1c4ca1f35
sushantsubedi30 <sushantsubedi352@gmail.com>
2026-07-08 22:15:16 +0545

    First commit

---
+++ Question1.cs
@@ -0,0 +1,19 @@
+//Declare variables to store your name,age and city, then display them.
+using System;
+
+ class Question1
+{
+     public static void Display()
+     {
+        string name;
+        int age;
+        string city;
+
+        name ="Sushantsubedi";
+        age =17;
+        city="Pokhara";
+
+        Console.WriteLine($"Name is {name} Age is {age} city is {city}"); 
+}
+
+}
