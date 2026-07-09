459135fdd0fa45281962bafb96c16b6dbc5a53da
sushansubedi <bedisarkar36@gmail.com>
2026-07-09 16:07:29 +0545

    update

--- Question5.cs
+++ Question5.cs
@@ -1,5 +1,15 @@
+//Swap the values of two variables using a third variable.
 using System;
 class Question5
 {
     public static void Main()
+    {
+        int a=2;
+        int b=3;
+        int c;
+         c =b;
+         b=a;
+      a=c;
+        Console.WriteLine($"the swap value of two variable {a} and {b}");
+    }
 }
