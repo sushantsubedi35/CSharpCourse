a4c63d5f93bbffc8872467e5f4d73fb31fb94787
sushantsubedi30 <sushantsubedi352@gmail.com>
2026-07-09 07:39:04 +0545

    Added Question3  program

---
+++ Question3.cs
@@ -0,0 +1,13 @@
+//Declare three variables for Physics, Chemistry, and Mathematics marks, then calculate the total.
+using System;
+ class Question3
+{
+    public static void Main()
+    {
+        int Physics =44;
+        int Chemistry =33;
+        int Mathematics =22;
+         
+         Console.WriteLine(Physics+Chemistry+Mathematics);
+    }
+}
---
+++ Question4.cs
@@ -0,0 +1,8 @@
+//Store the radius of a circle in a variable and calculate the area.
+using system;
+class Question4
+{
+    public static void Main()
+
+    
+}
